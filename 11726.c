16dd7f0114d891e84b4abef51c0ce424725d649e
kogo8976 <64471177+kogo8976@users.noreply.github.com>
2021-03-03 20:49:23 +0900

    DP

---
+++ 11726.c
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int d[1001];
+
+int	main(void)
+{
+	int n;
+
+	scanf("%d", &n);
+	d[1] = 1;
+	d[2] = 2;
+	for (int i = 3; i <= n; i++)
+	{
+		d[i] = (d[i - 1] + d[i - 2]) % 10007;
+	}
+	printf("%d", d[n]);
+	return (0);
+}
