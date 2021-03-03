e58022c1b8406ae2ca2437026efd3696dc822e17
kogo8976 <64471177+kogo8976@users.noreply.github.com>
2021-03-03 20:58:16 +0900

    DP

--- 11053.c
+++ 11053.c
@@ -1,30 +1,22 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-#define max(a, b) ((a) > (b) ? (a) : (b))
-
-int a[1001];
 int d[1001];
 
 int	main(void)
 {
-	int n;
-	int result = 0;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &a[i]);
-	for (int i = 0; i < n; i++)
-	{
-		d[i] = 1;
-		for (int j = 0; j < i; j++)
-		{
-			if (a[i] > a[j] && d[j] + 1 > d[i])
-				d[i] = d[j] + 1;
-		}
-	}
-	for (int i = 0; i < n; i++)
-		result = max(result, d[i]);
-	printf("%d", result);
-	return (0);
+	int n, t;
+
 	
+	scanf("%d", &t);
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 4;
+	while (t--)
+	{
+		scanf("%d", &n);
+		for (int i = 4; i <= n; i++)
+			d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+		printf("%d\n", d[n]);
+	}
+	return (0);
 }
