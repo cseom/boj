205a74cb9ceb95c314a7af5b2384cfd957a456fe
seko8976 <64471177+seko8976@users.noreply.github.com>
2021-07-27 21:08:09 +0900

    back tracking

---
+++ 15650.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+void sol(int n, int k, int cnt, bool check[10], vector<int> &ans)
+{
+	if (cnt == k)
+	{
+		for (int i = 0; i < ans.size(); i++)
+			cout << ans[i] << ' ';
+		cout << '\n';
+		return ;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		if (!ans.empty() && ans.back() >= i)
+			continue;
+		check[i] = true;
+		ans.push_back(i);
+		sol(n, k, cnt + 1, check, ans);
+		check[i] = false;
+		ans.pop_back();
+	}
+}
+
+int main()
+{
+	int n, m;
+	bool check[10];
+	vector<int> ans;
+
+	cin >> n >> m;
+	memset(check, false, sizeof(check));
+	sol(n, m, 0, check, ans);
+	return (0);
+}
